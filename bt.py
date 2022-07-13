03ce380853d6f8693da34415fcefb573289bf83d
Sehaj-09 <54837609+Sehaj-09@users.noreply.github.com>
2022-07-13 16:59:21 +0530

    Update bt.py

--- bt.py
+++ bt.py
@@ -16,15 +16,16 @@ def printTree(root):
     printTree(root.left)
     printTree(root.right)
     
-btn1=Node(1)
-btn2=Node(2)
-btn3=Node(3)
-btn4=Node(4)
-btn5=Node(5)
+def treeInput():
+    rootData=int(input())
+    if rootData == -1:
+        return None
+    root = Node(rootData)
+    leftTree=treeInput()
+    rightTree=treeInput()
+    root.left=leftTree
+    root.right=rightTree
+    return root
 
-
-btn1.left=btn2
-btn1.right=btn3
-btn2.left=btn4
-btn2.right=btn5
-    
+root=treeInput()
+printTree(root)
